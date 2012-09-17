05f92f06caa39841f04e310b74dd2c24e54c4f19
Evgeniy Tkachenko <e.jeka.mail@gmail.com>
2012-09-17 13:49:43 +0400

    Update PagesFromTemplates/functions.php

--- PagesFromTemplates/functions.php
+++ PagesFromTemplates/functions.php
@@ -1,29 +1,4 @@
 <?php
-/*
-Plugin Name: Pages From Templates
-Plugin URI:
-Description: Создание страниц с использованием шаблонов
-Version: 1.0
-Author:  Tkachenko Evgeniy
-Author URI:
-*/
-
-/*  Copyright 2012  Tkachenko Evgeniy  (email: e.jeka.mail@gmail.com)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
 
 if(!is_admin())
     return;
